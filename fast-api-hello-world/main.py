e2d147d8dd81fda9f216c317942ace54a645cdc6
nroldanf <nicolas.roldan@mail.escuelaing.edu.co>
2022-07-16 17:20:07 -0500

    add examples for swagger ui testing

--- fast-api-hello-world/main.py
+++ fast-api-hello-world/main.py
@@ -1,8 +1,8 @@
 from typing import Optional
 from enum import Enum
 from unittest.mock import Base
-from pydantic import BaseModel
-from pydantic import Field
+from pydantic import BaseModel, validator
+from pydantic import Field, EmailStr, HttpUrl
 from fastapi import FastAPI
 from fastapi import Body, Query, Path
 
@@ -22,25 +22,90 @@ class Person(BaseModel):
     first_name: str = Field(
         ..., 
         min_length=1,
-        max_length=50
+        max_length=50,
+        example="Matilde"
     )
     last_name: str = Field(
         ..., 
         min_length=1,
-        max_length=50
+        max_length=50,
+        example="Fajardo"
     )
     age: int = Field(
         ...,
         gt=0,
-        le=100
+        le=100,
+        example=20,
+    )
+    password: str = Field(
+        min_length=10, 
+        max_length=20,
+        example="123abcdefgg"
     )
     is_married: Optional[bool] = Field(default=None)
     hair_color: Optional[HairColor] = Field(default=None)
+    email: Optional[EmailStr] = Field(
+        default=None,
+        example="nicolas@gmail.com"
+    )
+    web_page: Optional[HttpUrl] = Field(
+        default=None,
+        example="google.com"
+    )
+    
+    # custom validation
+    @validator('password')
+    def passwords_match(cls, v):
+        assert v.isalnum(), 'must be alphanumeric'
+        return v
+
+    # define default values to test the API in Swagger UI
+    # must be named "example" for swagger to recognize it
+    
+    # class Config:
+    #     schema_extra = {
+    #         "example": {
+    #             "first_name": "Nicolás",
+    #             "last_name": "Roldán",
+    #             "age": 20,
+    #             "password": "123abcdefgg",
+    #             "hair_color": "blonde",
+    #             "web_page": "google.com",
+    #             "email": "nicolas@gmail.com"
+    #         }
+    #     }
 
 class Location(BaseModel):
-    city: str
-    state: str
-    country: str
+    city: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        example="Bogotá"
+    )
+    state: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        example="Cundinamarca"
+    )
+    country: str = Field(
+        ...,
+        min_length=1,
+        max_length=50,
+        example="Colombia"
+    )
+    
+    # Alternative way for defining default values for examples in
+    # Swaggr UI.
+    
+    # class Config:
+    #     schema_extra = {
+    #         "example": {
+    #             "city": "Bogotá",
+    #             "state": "Cundinamarca",
+    #             "country": "Colombia",
+    #         }
+    #     }
 
 # path operation
 @app.get("/")
