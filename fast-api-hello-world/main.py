4740a445dc8e21b520e809422fa7b5b5b434b29a
nroldanf <nicolas.roldan@mail.escuelaing.edu.co>
2022-07-16 16:04:13 -0500

    add query params validations

--- fast-api-hello-world/main.py
+++ fast-api-hello-world/main.py
@@ -1,7 +1,7 @@
 from typing import Optional
 from pydantic import BaseModel
 from fastapi import FastAPI
-from fastapi import Body
+from fastapi import Body, Query
 
 # Contains the application
 app = FastAPI()
@@ -22,3 +22,11 @@ def home():
 @app.post("/person/new")
 def create_person(person: Person = Body(...)):
     return person
+
+# query params validations
+@app.get("/person/detail")
+def show_person(
+    name: Optional[str] = Query(None, min_length=1, max_length=50),
+    age: int = Query(...)
+):
+    return {name: age}
